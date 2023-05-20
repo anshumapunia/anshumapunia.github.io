66d5edab6940acdb18524a2afa6729ee6a5dd5cb
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-05-20 08:10:32 +0530

    portfolio

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 // let resume1=document.getElementById("resumelink-1").addEventListener("click",opendrive);
 // let resume2=document.getElementById("resume-link-2").addEventListener("click",opendrive);
 
-function opendrive(){
-    window.open(
-        (href="https://drive.google.com/file/d/1POjdPMFjOFN0KSQypUovvnSSv1H9Czae/view?usp=share_link")
-    )
-}
+// function opendrive(){
+//     window.open(
+//         (href="https://drive.google.com/file/d/1POjdPMFjOFN0KSQypUovvnSSv1H9Czae/view?usp=share_link")
+//     )
+// }
 
-let hamburger=document.querySelector(".hamburger")
-hamburger.onclick=()=>{
-    let navlist=document.querySelector(".nav-list")
-    navlist.classList.toggle("active");
-}
+// let hamburger=document.querySelector(".hamburger")
+// hamburger.onclick=()=>{
+//     let navlist=document.querySelector(".nav-list")
+//     navlist.classList.toggle("active");
+// }
 
 // let typed= new typed(".typing",{
 //     String:["Full Stack Developer"],
