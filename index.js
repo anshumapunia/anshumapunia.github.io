ee4758f6c42391673773d40fc704ea84aac3c804
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-05-24 07:04:16 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
   var textElement = document.querySelector('.fade-in-fade-out');
-  var texts = ['Anshuma Devi', 'Web Developer', 'Codder', 'freelencer'];
+  var texts = ['Anshuma Devi', 'Web Developer', 'Codder', 'Backend Programmer'];
   var currentTextIndex = 0;
 
   function changeText() {
     textElement.textContent = texts[currentTextIndex];
     currentTextIndex = (currentTextIndex + 1) % texts.length;
   }
-
+  changeText();
   setInterval(changeText, 3000);
 });
