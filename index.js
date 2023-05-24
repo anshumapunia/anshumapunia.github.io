6455f7a119831e8b2e488ba97541c2708e45ba49
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-05-24 06:45:12 +0530

    porfolio

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-// resume button
+window.addEventListener('DOMContentLoaded', function() {
+  var textElement = document.querySelector('.fade-in-fade-out');
+  var texts = ['Anshuma Devi', 'Web Developer', 'Codder', 'freelencer'];
+  var currentTextIndex = 0;
 
-// let resume1=document.getElementById("resumelink-1").addEventListener("click",opendrive);
-// let resume2=document.getElementById("resume-link-2").addEventListener("click",opendrive);
+  function changeText() {
+    textElement.textContent = texts[currentTextIndex];
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
+  }
 
-// function opendrive(){
-//     window.open(
-//         (href="https://drive.google.com/file/d/1POjdPMFjOFN0KSQypUovvnSSv1H9Czae/view?usp=share_link")
-//     )
-// }
-
-// let hamburger=document.querySelector(".hamburger")
-// hamburger.onclick=()=>{
-//     let navlist=document.querySelector(".nav-list")
-//     navlist.classList.toggle("active");
-// }
-
-// let typed= new typed(".typing",{
-//     String:["Full Stack Developer"],
-//     typespeed:100,
-//     backspeed:60,
-//     loop:true
-// })
-
+  setInterval(changeText, 3000);
+});
