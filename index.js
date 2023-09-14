881eee88f82515671a3c36ebd3a0ee2917da89f5
anshumapunia123 <112813318+anshumapunia123@users.noreply.github.com>
2023-09-14 17:29:22 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,29 +1,73 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var textElement = document.querySelector('.fade-in-fade-out');
-  var texts = ['Anshuma Devi', 'Web Developer', 'Codder', 'Backend Programmer'];
-  var currentTextIndex = 0;
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky nav-menu on scroll script
+    if (this.scrollY > 20) {
+      $(".nav-menu").addClass("sticky");
+    } else {
+      $(".nav-menu").removeClass("sticky");
+    }
 
-  function changeText() {
-    textElement.textContent = texts[currentTextIndex];
-    currentTextIndex = (currentTextIndex + 1) % texts.length;
-  }
-  changeText();
-  setInterval(changeText, 3000);
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $("#nav-menu .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/nav-menu script
+  $(".menu-btn").click(function () {
+    $("#nav-menu .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Web-Developer", "Codder", "Backend-Programmer","Front-End Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Web-Developer", "Codder"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
- 
-function downloadResume1() {
-  var resumeLink = document.getElementById("resume-link-2");
-  var resumePDF = resumeLink.href;
-  window.open(resumePDF, "_blank");
-}
-
-document.getElementById("resume-button-2").addEventListener("click", downloadResume1);
-
-
-function downloadResume() {
-  var resumeLink = document.getElementById("resume-link-1");
-  var resumePDF = resumeLink.href;
-  window.open(resumePDF, "_blank");
-}
-
-document.getElementById("resume-link-1").addEventListener("click", downloadResume);
